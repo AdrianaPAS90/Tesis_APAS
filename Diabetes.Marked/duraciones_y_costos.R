b2fce57012555b2a3e31442872e0bd9ffcd0a47d
AdrianaPAS90 <apas90@hotmail.com>
2017-12-01 16:20:26 -0600

    .

--- Diabetes.Marked/duraciones_y_costos.R
+++ Diabetes.Marked/duraciones_y_costos.R
@@ -4,11 +4,26 @@ tabla_inicial_NHS<-read.csv("diabetes_marked_datos_sim.csv", header = TRUE)
 
 #Creacion de la base de datos
 df_NHS<-data.frame(tabla_inicial_NHS)
-paciente<-as.factor(df_NHS$paciente)
-n_pacientes<-500
+df_NHS$paciente<-as.factor(df_NHS$paciente)
+n<-nrow(df_NHS)
+
+#Calculo de los costos agregados
+aux<-array(NaN,n)
+aux[1]<-df_NHS$presc.cost[1]
+for(i in 2:n){
+  if(df_NHS$paciente[i] == df_NHS$paciente[i-1]){
+    aux[i]<-aux[i-1] + df_NHS$presc.cost[i]
+  }
+  else{
+    aux[i]<-df_NHS$presc.cost[i]
+  }
+}
+
+c<-cbind(df_NHS, aux)
 
 #Calculo de duraciones
-dur<-tapply(df_NHS$presc.code, df_NHS$paciente, rle)
+df_NHS$paciente<-as.factor(c$paciente)
+dur<-tapply(c$presc.code, df_NHS$paciente, rle)
 
 i<-1
 duraciones<-data.frame("paciente"=rep(i,length(dur[[i]][1]$lengths)),"tratamiento"=dur[[i]][2],"duracion"=dur[[i]][1])
@@ -16,3 +31,13 @@ duraciones<-data.frame("paciente"=rep(i,length(dur[[i]][1]$lengths)),"tratamient
 for(i in 2:n_pacientes){
   duraciones<-rbind(duraciones,data.frame("paciente"=rep(i,length(dur[[i]][1]$lengths)),"tratamiento"=dur[[i]][2],"duracion"=dur[[i]][1]))
 }
+
+#Calculo de costos agregados
+#cost<-array(NaN,dim(dur))
+#ca<-as.double(c$aux)
+#cost[1]<-ca[1]
+#j<-1
+
+#cost<-tapply(c$aux, tapply(c$presc.code, df_NHS$paciente, rle), rle)
+#Calculo de costos agregados
+#cost<-tapply(df_NHS$presc.cost, df_NHS$paciente, cumsum)
