ee2ea0e511d1b37bbdbdc8cbe22d76535d2e01ad
Juan Carlos Martínez-Ovando <jc.martinez.ovando@gmail.com>
2018-06-04 12:50:50 -0500

    update tesis code

--- Code/bayesmpp_demo.R
+++ Code/bayesmpp_demo.R
@@ -1,1 +1,21 @@
-bayesmpp_demo.R
+#bayesmpp_demo.R
+
+rm(list = ls())
+source("./Code/slice.sampler/uni.slice.R")
+
+datos<-read.csv("./Diabetes.Marked/diabetes_marked_datos_sim.csv", header = TRUE)
+
+source("./Code/bayesmpp.R")
+
+# Prior
+alpha_0 = 2
+beta_0 = 0.3
+
+# MCMC sampler
+M.sim <- 10
+
+bayesmpp_out <- bayesmpp(datos,M.sim,alpha_0=2,beta_0=0.3)
+  
+###
+
+
