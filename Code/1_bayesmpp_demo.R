e593c23cf2b97779d906dcfe746853d79ceade0a
AdrianaPAS90 <apas90@hotmail.com>
2018-06-28 14:16:19 -0500

    .

--- Code/1_bayesmpp_demo.R
+++ Code/1_bayesmpp_demo.R
@@ -24,3 +24,16 @@ save(datos,
      bayesmpp_out,
      file="./Diabetes.Marked/bayesmpp_out.RData")
 
+analisis_estimacion<-function(z){
+  g1 <- ggplot(data.frame(est=1:length(z),z=z),aes(x=est,y=z))+
+    geom_line() 
+  g2 <- ggplot(data.frame(est=1:length(z),z=z),aes(x=est,
+                                                   y=cumsum(z)/(1:length(z))))+
+    geom_line()
+}
+
+#---Lo que queremos es que tome cada uno de los parámetros de bayesmpp_out, para hacer estas
+#gráficas y determinar los valores estimados de los parámetros y las variables latentes.
+#out <- bayesmpp_out 
+#z <- out$deviance
+#analisis_estimacion(z)
